45ac682e862ebd1136d95e770addf69020230883
wenxi0510 <wenxi0510@gmail.com>
2025-04-03 03:32:21 +0800

    Delete For combination directory

--- For combination/InventoryManagement.java
+++
@@ -1,5 +0,0 @@
-public interface InventoryManagement {
-    void addMedication();
-    void removeMedication();
-    void updateStock();
-}
--- For combination/Replenish.java
+++
@@ -1,6 +0,0 @@
-public interface Replenish{
-    void updateLowStockAlert();
-    boolean viewReplenishmentRequests();
-    void newReplenishmentRequest(String medicationID, int replenishAmount);
-    void fulfillReplenishmentRequest(String medicationID);
-}
